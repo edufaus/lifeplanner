3910244a279bfeef45ea0845297d0f8c0aee68cf
Eduard Faus <efaus25@belenwolverines.org>
2022-12-30 18:00:19 +0100

    Update firestore.js

--- src/routes/room/firestore.js
+++ src/routes/room/firestore.js
@@ -5,13 +5,8 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 let firebaseConfig = {
-  apiKey: "AIzaSyAouayCtY1vfQYR8MJDn_AMrkEDEPYsyNk",
-  authDomain: "lifeplanner-a1cea.firebaseapp.com",
-  projectId: "lifeplanner-a1cea",
-  storageBucket: "lifeplanner-a1cea.appspot.com",
-  messagingSenderId: "681439159972",
-  appId: "1:681439159972:web:681db05832924401632831"
 };
+// Firebase Config
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
